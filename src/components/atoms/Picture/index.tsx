c8bd680610363704c079b9b730963500aa851716
Manoel Duran <manoel.duran@hotmail.com>
2022-12-12 18:22:16 -0300

    added href to maps

--- src/components/atoms/Picture/index.tsx
+++ src/components/atoms/Picture/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Image from 'next/image';
 import { PictureDTO } from "../../../dtos/PictureDTO";
 
-const Picture: React.FC<PictureDTO> = ({ source, alt }) => {
+const Picture: React.FC<PictureDTO> = ({ source, alt}) => {
     return (
-        <Image src={source} alt={alt} style={{width: "350px" , height: "350px", borderRadius: "100%"}}/>
+        <Image src={source} alt={alt}  style={{width: "350px" , height: "350px", borderRadius: "100%"}}/>
     )
 }
 
--- src/components/organisms/Clinic/index.tsx
+++ src/components/organisms/Clinic/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { ExperienceText } from "../../atoms/ExperienceText";
 import { SectionTitle } from "../../atoms/SectionTitle";
@@ -8,15 +9,19 @@ const Clinic: React.FC = () => {
     return (
         <Container>
             <SectionTitle
-            title="Consultórios"
+                title="Consultórios"
             />
             <Content>
-                <ExperienceText
-                text="Rua Arthur de Azevêdo Machado, 1459, Salvador"
-                />
+
+                <a href="https://www.google.com/maps/place/12%C2%B059'13.6%22S+38%C2%B026'55.5%22W/@-12.9871101,-38.4487495,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xf0b9310237e2ece5!8m2!3d-12.9871101!4d-38.4487495">
+                    <ExperienceText
+                        text="Rua Arthur de Azevêdo Machado, 1459, Salvador"
+                    />
+                </a>
+
             </Content>
         </Container>
     );
 };
 
-export {Clinic};
+export { Clinic };
